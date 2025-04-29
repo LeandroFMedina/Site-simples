6e8772b25c507a9d98cb256797f7fe51510617d7
LeandroFMedina <leandrofiorenthino50@gmail.com>
2025-04-29 17:15:54 -0300

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-function mostrarMensagem() {
-    document.getElementById("mensagem").textContent = "Viajar de moto é, assim, buscar longe algo que nos surpreenda, aquela paisagem que nos lembra as linhas de um poema ou a música que mais gostamos e que ficará gravada para sempre em nossa memória.";
-  }
